93a1d998888c19ec1c10e37bd718d8cf2aab6414
dodos <theodoros.a.minas@gmail.com>
2025-02-26 15:02:15 +0100

    "pipes "

--- src/main/java/com/dodos/springboot/demo/dodosapp/rest/FunRestControler.java
+++ src/main/java/com/dodos/springboot/demo/dodosapp/rest/FunRestControler.java
@@ -1,14 +1,28 @@
 package com.dodos.springboot.demo.dodosapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestControler {
+    @Value("${coach.value}")
+    private String name;
 
     @GetMapping("/")
     public String hello(){
-        return "pipes pantou";
+
+        return "pipes pantou "+name;
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
     }
 }
