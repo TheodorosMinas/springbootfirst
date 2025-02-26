15d16a98fd5938e856030f463ba83cd37dbe4902
dodos <theodoros.a.minas@gmail.com>
2025-02-26 12:15:29 +0100

    springboo project

---
+++ src/main/java/com/dodos/springboot/demo/dodosapp/DodosappApplication.java
@@ -0,0 +1,13 @@
+package com.dodos.springboot.demo.dodosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodosappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodosappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodos/springboot/demo/dodosapp/DodosappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodos.springboot.demo.dodosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodosappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
